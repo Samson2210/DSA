84eee633b85c4585cfee0759e70affa5d4134385
Samson <samsonsurin22@gmail.com>
2023-09-30 10:17:32 +0530

    emplemented postfix evalution

--- infix_to_postfix/infix_to_postfix.c
+++ infix_to_postfix/infix_to_postfix.c
@@ -1,47 +1,75 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "infix_to_postfix.h"
 #include "../stack/stack.h"
 
-//return postfix expression
-char* infixToPostfix(char * infix){
+int isOperator(char ch)
+{
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+
+int precedence(char operator)
+{
+    if (operator== '+' || operator== '-')
+        return 1;
+    if (operator== '*' || operator== '/')
+        return 2;
+    if (operator== '^')
+        return 3;
+    return 0;
+}
+
+// return postfix expression
+char *infixToPostfix(char *infix)
+{
     int j = 0;
     int len = strlen(infix);
-    char* postfix = (char*)malloc(sizeof(char)*len);
-    stack* s = createStack();
+    char *postfix = (char *)malloc(sizeof(char) * len);
+    stack *s = createStack();
 
-    for(int i=0;i<len;i++){
+    for (int i = 0; i < len; i++)
+    {
         if (infix[i] == ' ' || infix[i] == '\t')
             continue;
 
-        if(isalnum(infix[i])){
+        if (isalnum(infix[i]))
+        {
             postfix[j++] = infix[i];
         }
 
-        else if(infix[i]=='('){
-            push(s,'(');
+        else if (infix[i] == '(')
+        {
+            push(s, '(');
         }
 
-        else if(infix[i]==')'){
-            while(!isEmpty(s) && peek(s)!='('){
+        else if (infix[i] == ')')
+        {
+            while (!isEmpty(s) && peek(s) != '(')
+            {
                 postfix[j++] = pop(s);
             }
-            if(!isEmpty(s) && peek(s) !='(')
-            return "Invalid expression";
+            if (!isEmpty(s) && peek(s) != '(')
+                return "Invalid expression";
             else
-                pop(s);            
+                pop(s);
         }
 
-        else if(isOperator(infix[i])){
-            while(!isEmpty && precedence(peek(s))>=precedence(infix[i]))
-            postfix[j++] =  pop(s);
-            push(s,infix[i]);
+        else if (isOperator(infix[i]))
+        {
+            while (!isEmpty(s) && precedence(infix[i])<= precedence(peek(s))){
+                postfix[j++] = pop(s);
+            }
+            push(s, infix[i]);
         }
+
     }
 
-    while(!isEmpty(s)){
-        if(peek(s)== '('){
+    while (!isEmpty(s))
+    {
+        if (peek(s) == '(')
+        {
             return "Invalid Expression";
         }
         postfix[j++] = pop(s);
